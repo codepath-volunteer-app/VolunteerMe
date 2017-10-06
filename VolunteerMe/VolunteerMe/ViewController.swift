63924df2ddd9f97cf372ce41e6f10f3fa7e77ba6
ruthie_berman <ruthienaomi@gmail.com>
2017-10-06 10:28:27 -0700

    Created initial project; installed needed podfiles

---
+++ VolunteerMe/VolunteerMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VolunteerMe
+//
+//  Created by ruthie_berman on 10/6/17.
+//  Copyright © 2017 volunteer_me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
