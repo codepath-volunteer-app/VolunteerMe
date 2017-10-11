a2614a37229cbee7871a09597a0f0f8085713aec
Rajat Bhargava <rbhargava@twitter.com>
2017-10-11 15:53:21 -0700

    FeedTableView

---
+++ VolunteerMe/VolunteerMe/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  VolunteerMe
+//
+//  Created by Rajat Bhargava on 10/11/17.
+//  Copyright © 2017 volunteer_me. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ VolunteerMe/VolunteerMe/VolunteerFeedViewController.swift
@@ -0,0 +1,58 @@
+//
+//  VolunteerFeedViewController.swift
+//  VolunteerMe
+//
+//  Created by Rajat Bhargava on 10/11/17.
+//  Copyright © 2017 volunteer_me. All rights reserved.
+//
+
+import UIKit
+
+class VolunteerFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Configure tableView.
+        tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Table View Delegate
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell") as! FeedCell
+        //cell.colorLabel.text = data[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Table View Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
