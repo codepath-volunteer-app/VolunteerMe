434e0ac647409804f849d271a45fdcff8b9764c9
ruthie_berman <ruthienaomi@gmail.com>
2017-10-23 22:34:27 -0700

    login and create account working

--- VolunteerMe/VolunteerMe/CreateAccountViewController.swift
+++ VolunteerMe/VolunteerMe/CreateAccountViewController.swift
@@ -9,112 +9,126 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-    
-    @IBOutlet weak var titleText: UILabel!
-    @IBOutlet weak var subtitleText: UILabel!
-    @IBOutlet weak var usernameField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var usernameUnderline: UIView!
-    @IBOutlet weak var passwordUnderline: UIView!
-    @IBOutlet weak var createAccountButtonBackground: UIView!
-    @IBOutlet weak var createAccountButtonLabel: UILabel!
-    @IBOutlet weak var tagsView: UIView!
 
-    @IBOutlet weak var animalsTag: UILabel!
-    @IBOutlet weak var discriminationTag: UILabel!
-    @IBOutlet weak var educationTag: UILabel!
-    @IBOutlet weak var elderlyTag: UILabel!
-    @IBOutlet weak var environmentTag: UILabel!
-    @IBOutlet weak var healthTag: UILabel!
-    @IBOutlet weak var homelessnessTag: UILabel!
-    @IBOutlet weak var hungerTag: UILabel!
-    @IBOutlet weak var youthTag: UILabel!
+  @IBOutlet weak var titleText: UILabel!
+  @IBOutlet weak var subtitleText: UILabel!
+  @IBOutlet weak var usernameField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  @IBOutlet weak var usernameUnderline: UIView!
+  @IBOutlet weak var passwordUnderline: UIView!
+  @IBOutlet weak var createAccountButtonBackground: UIView!
+  @IBOutlet weak var createAccountButtonLabel: UILabel!
+  @IBOutlet weak var tagsView: UIView!
 
-    
-    var tagLabels: [UILabel] = [UILabel]()
+  @IBOutlet weak var animalsTag: UILabel!
+  @IBOutlet weak var discriminationTag: UILabel!
+  @IBOutlet weak var educationTag: UILabel!
+  @IBOutlet weak var elderlyTag: UILabel!
+  @IBOutlet weak var environmentTag: UILabel!
+  @IBOutlet weak var healthTag: UILabel!
+  @IBOutlet weak var homelessnessTag: UILabel!
+  @IBOutlet weak var hungerTag: UILabel!
+  @IBOutlet weak var youthTag: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        titleText.textColor = Color.PRIMARY_COLOR
-        subtitleText.textColor = Color.PRIMARY_COLOR
-        usernameUnderline.backgroundColor = Color.PRIMARY_COLOR
-        passwordUnderline.backgroundColor = Color.PRIMARY_COLOR
-        
-        createAccountButtonBackground.backgroundColor = Color.PRIMARY_COLOR
-        createAccountButtonBackground.layer.cornerRadius = 8.0
-        createAccountButtonBackground.clipsToBounds = true
-        createAccountButtonLabel.textColor = Color.WHITE
+  var tagLabels: [UILabel] = [UILabel]()
 
-        tagLabels = [
-          animalsTag,
-          discriminationTag,
-          educationTag,
-          elderlyTag,
-          environmentTag,
-          healthTag,
-          homelessnessTag,
-          hungerTag,
-          youthTag,
-        ]
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    titleText.textColor = Color.PRIMARY_COLOR
+    subtitleText.textColor = Color.PRIMARY_COLOR
+    usernameUnderline.backgroundColor = Color.PRIMARY_COLOR
+    passwordUnderline.backgroundColor = Color.PRIMARY_COLOR
 
-        var i = 0
-        for tag in tagLabels {
-          tag.tag = i
-          tag.backgroundColor = Color.PRIMARY_COLOR
-          tag.textColor = Color.WHITE
-          tag.text = " " + tag.text! + " "
-          tag.layer.cornerRadius = 8.0
-          tag.clipsToBounds = true
-          tag.isUserInteractionEnabled = true
-          let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tagTapped(gestureRecognizer:)))
-          tag.addGestureRecognizer(tapGesture)
-          i += 1
-        }
-        
-        // Do any additional setup after loading the view.
+    createAccountButtonBackground.backgroundColor = Color.PRIMARY_COLOR
+    createAccountButtonBackground.layer.cornerRadius = 8.0
+    createAccountButtonBackground.clipsToBounds = true
+    createAccountButtonLabel.textColor = Color.WHITE
+
+    tagLabels = [
+      animalsTag,
+      discriminationTag,
+      educationTag,
+      elderlyTag,
+      environmentTag,
+      healthTag,
+      homelessnessTag,
+      hungerTag,
+      youthTag,
+    ]
+
+    var i = 0
+    for tag in tagLabels {
+      tag.tag = i
+      tag.backgroundColor = Color.PRIMARY_COLOR
+      tag.textColor = Color.WHITE
+      tag.text = " " + tag.text! + " "
+      tag.layer.cornerRadius = 8.0
+      tag.clipsToBounds = true
+      tag.isUserInteractionEnabled = true
+      let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tagTapped(gestureRecognizer:)))
+      tag.addGestureRecognizer(tapGesture)
+      i += 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // Do any additional setup after loading the view.
+  }
 
-    func tagTapped(gestureRecognizer: UITapGestureRecognizer) {
-      let tag = tagLabels[gestureRecognizer.view!.tag]
-      if tag.backgroundColor == Color.PRIMARY_COLOR {
-        tag.backgroundColor = Color.SECONDARY_COLOR
-      } else {
-        tag.backgroundColor = Color.PRIMARY_COLOR
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func tagTapped(gestureRecognizer: UITapGestureRecognizer) {
+    let tag = tagLabels[gestureRecognizer.view!.tag]
+    if tag.backgroundColor == Color.PRIMARY_COLOR {
+      tag.backgroundColor = Color.SECONDARY_COLOR
+    } else {
+      tag.backgroundColor = Color.PRIMARY_COLOR
+    }
+    // Mark tag as selected
+  }
+
+  @IBAction func cancelButtonTapped(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
+
+  @IBAction func createAccountButtonTapped(_ sender: Any) {
+    // Create account
+    var selectedTags: [String] = [String]()
+    for tag in tagLabels {
+      if tag.backgroundColor == Color.SECONDARY_COLOR {
+        selectedTags.append(tag.text!)
       }
-      // Mark tag as selected
     }
 
-    @IBAction func cancelButtonTapped(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    if usernameField.text == nil || usernameField.text == "" {
+      createAlert(title: "Oops!", message: "Please choose a username.")
+    } else if passwordField.text == nil || passwordField.text == "" {
+      createAlert(title: "Oops!", message: "Please choose a password.")
+    } else if selectedTags.count == 0 {
+      createAlert(title: "Oops!", message: "Don't forget to select a few categories you're interested in!")
+    } else {
+      User.createNewUser(username: usernameField.text!, password: passwordField.text!, name: usernameField.text, userDescription: nil, userType: .Volunteer, profilePictureUrl: nil, tags: selectedTags) { (user) in
+        self.performSegue(withIdentifier: "CreatedAccountSegue", sender: self)
+      }
     }
-    
-    @IBAction func createAccountButtonTapped(_ sender: Any) {
-        // Create account
-        var selectedTags: [String] = [String]()
-        for tag in tagLabels {
-            if tag.backgroundColor == Color.SECONDARY_COLOR {
-                selectedTags.append(tag.text!)
-            }
-        }
-        User.createNewUser(username: usernameField.text!, password: passwordField.text!, name: usernameField.text, userDescription: nil, userType: .Volunteer, profilePictureUrl: nil, tags: selectedTags) { (user) in
-            self.dismiss(animated: true, completion: nil)
-        }
-        
-    }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+  }
+
+  func createAlert(title: String, message: String) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+    alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+    self.present(alertController, animated: true, completion: nil)
+  }
+
+  /*
+   // MARK: - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+
 }
 
--- VolunteerMe/VolunteerMe/LoginViewController.swift
+++ VolunteerMe/VolunteerMe/LoginViewController.swift
@@ -9,76 +9,82 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    @IBOutlet weak var titleText: UILabel!
-    @IBOutlet weak var subtitleText: UILabel!
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var usernameUnderline: UIView!
-    @IBOutlet weak var passwordUnderline: UIView!
-    @IBOutlet weak var loginButtonBackground: UIView!
-    @IBOutlet weak var loginButtonText: UILabel!
-    @IBOutlet weak var createAccountButtonBackground: UIView!
-    @IBOutlet weak var createAccountButtonText: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        titleText.textColor = Color.PRIMARY_COLOR
-        subtitleText.textColor = Color.PRIMARY_COLOR
-        usernameUnderline.backgroundColor = Color.PRIMARY_COLOR
-        passwordUnderline.backgroundColor = Color.PRIMARY_COLOR
-        
-        loginButtonBackground.backgroundColor = Color.PRIMARY_COLOR
-        loginButtonBackground.layer.cornerRadius = 8.0
-        loginButtonBackground.clipsToBounds = true
-        loginButtonText.textColor = Color.WHITE
-        
-        createAccountButtonBackground.backgroundColor = Color.WHITE
-        createAccountButtonBackground.layer.cornerRadius = 8.0
-        createAccountButtonBackground.layer.borderColor = Color.PRIMARY_COLOR.cgColor
-        createAccountButtonBackground.layer.borderWidth = 2.0
-        createAccountButtonBackground.clipsToBounds = true
-        createAccountButtonText.textColor = Color.PRIMARY_COLOR
-        
-        
-        
-        // Do any additional setup after loading the view.
+
+  @IBOutlet weak var titleText: UILabel!
+  @IBOutlet weak var subtitleText: UILabel!
+  @IBOutlet weak var usernameTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var usernameUnderline: UIView!
+  @IBOutlet weak var passwordUnderline: UIView!
+  @IBOutlet weak var loginButtonBackground: UIView!
+  @IBOutlet weak var loginButtonText: UILabel!
+  @IBOutlet weak var createAccountButtonBackground: UIView!
+  @IBOutlet weak var createAccountButtonText: UILabel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    titleText.textColor = Color.PRIMARY_COLOR
+    subtitleText.textColor = Color.PRIMARY_COLOR
+    usernameUnderline.backgroundColor = Color.PRIMARY_COLOR
+    passwordUnderline.backgroundColor = Color.PRIMARY_COLOR
+
+    loginButtonBackground.backgroundColor = Color.PRIMARY_COLOR
+    loginButtonBackground.layer.cornerRadius = 8.0
+    loginButtonBackground.clipsToBounds = true
+    loginButtonText.textColor = Color.WHITE
+
+    createAccountButtonBackground.backgroundColor = Color.WHITE
+    createAccountButtonBackground.layer.cornerRadius = 8.0
+    createAccountButtonBackground.layer.borderColor = Color.PRIMARY_COLOR.cgColor
+    createAccountButtonBackground.layer.borderWidth = 2.0
+    createAccountButtonBackground.clipsToBounds = true
+    createAccountButtonText.textColor = Color.PRIMARY_COLOR
+
+
+
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func loginButtonTapped(_ sender: Any) {
+    if usernameTextField.text == nil || usernameTextField.text == "" {
+      createAlert(title: "Oops!", message: "Please enter your username.")
+    } else if passwordTextField.text == nil || passwordTextField.text == "" {
+      createAlert(title: "Oops!", message: "Please enter your password.")
+    } else {
+      User.login(username: usernameTextField.text!, password: passwordTextField.text!) { (user) in
+        self.onSuccess()
+      }
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+
+  func createAlert(title: String, message: String) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+    alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+    self.present(alertController, animated: true, completion: nil)
+  }
+
+  func onSuccess() {
+    self.performSegue(withIdentifier: "LoginSegue", sender: self)
+  }
+
+
+  // MARK: - Navigation
+/*
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    if segue.identifier == "CreateAccountSegue" {
+      let navController = segue.destination as! UINavigationController
+      let createAccountController = navController.topViewController as! CreateAccountViewController
+      createAccountController.completionHandler = { self.onSuccess() }
     }
-    
-    @IBAction func loginButtonTapped(_ sender: Any) {
-        if usernameTextField.text == nil || usernameTextField.text == "" {
-            print("Missing username")
-            return
-        } else if passwordTextField.text == nil || passwordTextField.text == "" {
-            print("Missing password")
-            return
-        }
-        User.login(username: usernameTextField.text!, password: passwordTextField.text!) { (user) in
-            self.onSuccess()
-        }
-    }
-    
-    func onSuccess() {
-        self.performSegue(withIdentifier: "LoginSegue", sender: self)
-    }
-    
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-//        if segue.identifier == "CreateAccountSegue" {
-//            let navController = segue.destination as! UINavigationController
-//            let createAccountController = navController.topViewController as! CreateAccountViewController
-//            createAccountController.onSuccess = {() -> Void in self.onSuccess()}
-//        }
-//    }
+  }
+ */
 }
 
