c65bd7406454fbc1dec16027d055f5d924272f34
Auster Chen <auster.s.chen@gmail.com>
2017-10-30 22:31:31 -0700

    update organization create account tags

--- VolunteerMe/VolunteerMe/CreateAccountViewController.swift
+++ VolunteerMe/VolunteerMe/CreateAccountViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-  fileprivate static let mapOfTagStringsToTagNames = [
+  static let mapOfTagStringsToTagNames = [
     "Animals": "animals",
     "Education": "education",
     "Reading": "reading",
